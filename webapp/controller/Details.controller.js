0aefb2c1d0507e25800df4043062d6b6b214a97d
Franco Maciel <damianmaciel0@gmail.com>
2025-03-29 22:32:53 -0300

    add details page and redirect

---
+++ webapp/controller/Details.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "com/bootcamp/sapui5/finalproject/utils/Routes.utils"
+], (Controller, RoutesUtils) => {
+
+    return Controller.extend("com.bootcamp.sapui5.finalproject.controller.Details", {
+        onInit() {
+            const oRouteNames = RoutesUtils.getRouteNames()
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute(oRouteNames.details).attachPatternMatched(this._onObjectMatched, this);
+
+            console.log(this.getView().getModel())
+        },
+
+        _onObjectMatched: function (oEvent) {
+            const SupplierID = oEvent.getParameter("arguments").SupplierID;
+
+            this.getView().bindElement({
+                path: `/Suppliers(${SupplierID})`,
+                parameters: {
+                    expand: "Products"
+                }
+            });
+        },
+    });
+});
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,21 +1,16 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/base/Log",
-	"sap/ui/model/Sorter",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/format/DateFormat",
-	"sap/m/ToolbarSpacer",
 	"sap/ui/core/library",
-	"sap/ui/thirdparty/jquery",
-	"sap/ui/core/date/UI5Date",
     "com/bootcamp/sapui5/finalproject/utils/Home.utils",
     "com/bootcamp/sapui5/finalproject/utils/Routes.utils",
-], (Controller, Log, Sorter, JSONModel, DateFormat, ToolbarSpacer, CoreLibrary, jQuery, UI5Date, HomeUtils, RoutesUtils) => {
-    "use strict";
+	"sap/ui/table/RowAction",
+	"sap/ui/table/RowActionItem",
+], (Controller, CoreLibrary, HomeUtils, RoutesUtils, RowAction, RowActionItem,) => {
 	const SortOrder = CoreLibrary.SortOrder;
     return Controller.extend("com.bootcamp.sapui5.finalproject.controller.Home", {
 		onInit: async function() {
 			this.oModelNames = HomeUtils.getModelNames()
+			
             this.oRouteNames = RoutesUtils.getRouteNames()
 
             this.oRouter = this.getOwnerComponent().getRouter();
@@ -23,74 +18,57 @@ sap.ui.define([
             try {
                 const oDatos = await HomeUtils.getDataSuppliers([])
 
-
-
                 const [{ results }] = oDatos
-
-				console.log({ results })
 
                 await HomeUtils.setSuppliersModel(this, results);
             } catch (error) {
-				console.error(error, '----------------------------error')
                 MessageToast.show("Error al obtener los datos", {
                     width: "auto"
                 })
             }
-			// set explored app's demo model on this sample
-			// const oJSONModel = this.initSampleDataModel();
-			// const oView = this.getView();
-			// oView.setModel(oJSONModel);
 
-			// //Initial sorting
-			// const oProductNameColumn = oView.byId("name");
-			// oView.byId("table").sort(oProductNameColumn, SortOrder.Ascending);
+			const fnPress = this.onPressSupplierCell.bind(this);
 
-			// sap.ui.require(["sap/ui/table/sample/TableExampleUtils"], function(TableExampleUtils) {
-			// 	const oTb = oView.byId("infobar");
-			// 	oTb.addContent(new ToolbarSpacer());
-			// 	oTb.addContent(TableExampleUtils.createInfoButton("sap/ui/table/sample/Sorting"));
-			// }, function(oError) { /*ignore*/ });
+			this.modes = [
+				{
+					key: "Navigation",
+					text: "Navigation",
+					handler: () => {
+						const oTemplate = new RowAction({items: [
+							new RowActionItem({
+								type: "Navigation",
+								press: fnPress,
+								visible: "{Available}"
+							})
+						]});
+						return [1, oTemplate];
+					}
+				}
+			];
+
+			this.switchState("Navigation");
 		},
 
-		initSampleDataModel: function() {
-			// const oModel = new JSONModel();
+		switchState: function(sKey) {
+			const oTable = this.byId("table");
+			let iCount = 0;
+			let oTemplate = oTable.getRowActionTemplate();
+			if (oTemplate) {
+				oTemplate.destroy();
+				oTemplate = null;
+			}
 
-			// const oDateFormat = DateFormat.getDateInstance({source: {pattern: "timestamp"}, pattern: "dd/MM/yyyy"});
+			for (let i = 0; i < this.modes.length; i++) {
+				if (sKey === this.modes[i].key) {
+					const aRes = this.modes[i].handler();
+					iCount = aRes[0];
+					oTemplate = aRes[1];
+					break;
+				}
+			}
 
-			// jQuery.ajax(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"), {
-			// 	dataType: "json",
-			// 	success: function(oData) {
-			// 		const aTemp1 = [];
-			// 		const aTemp2 = [];
-			// 		const aSuppliersData = [];
-			// 		const aCategoryData = [];
-			// 		for (let i = 0; i < oData.ProductCollection.length; i++) {
-			// 			const oProduct = oData.ProductCollection[i];
-			// 			if (oProduct.SupplierName && aTemp1.indexOf(oProduct.SupplierName) < 0) {
-			// 				aTemp1.push(oProduct.SupplierName);
-			// 				aSuppliersData.push({Name: oProduct.SupplierName});
-			// 			}
-			// 			if (oProduct.Category && aTemp2.indexOf(oProduct.Category) < 0) {
-			// 				aTemp2.push(oProduct.Category);
-			// 				aCategoryData.push({Name: oProduct.Category});
-			// 			}
-			// 			oProduct.DeliveryDate = Date.now() - (i % 10 * 4 * 24 * 60 * 60 * 1000);
-			// 			oProduct.DeliveryDateStr = oDateFormat.format(UI5Date.getInstance(oProduct.DeliveryDate));
-			// 			oProduct.Heavy = oProduct.WeightMeasure > 1000 ? "true" : "false";
-			// 			oProduct.Available = oProduct.Status === "Available" ? true : false;
-			// 		}
-
-			// 		oData.Suppliers = aSuppliersData;
-			// 		oData.Categories = aCategoryData;
-
-			// 		oModel.setData(oData);
-			// 	},
-			// 	error: function() {
-			// 		Log.error("failed to load json");
-			// 	}
-			// });
-
-			// return oModel;
+			oTable.setRowActionTemplate(oTemplate);
+			oTable.setRowActionCount(iCount);
 		},
 
 		clearAllSortings: function(oEvent) {
@@ -99,69 +77,22 @@ sap.ui.define([
 			this._resetSortingState();
 		},
 
-		sortCategories: function(oEvent) {
-			// const oView = this.getView();
-			// const oTable = oView.byId("table");
-			// const oCategoriesColumn = oView.byId("categories");
-
-			// oTable.sort(oCategoriesColumn, this._bSortColumnDescending ? SortOrder.Descending : SortOrder.Ascending, /*extend existing sorting*/true);
-			// this._bSortColumnDescending = !this._bSortColumnDescending;
-		},
-
-		sortCategoriesAndName: function(oEvent) {
-			// const oView = this.getView();
-			// const oTable = oView.byId("table");
-			// oTable.sort(oView.byId("categories"), SortOrder.Ascending, false);
-			// oTable.sort(oView.byId("name"), SortOrder.Ascending, true);
-		},
-
-		sortDeliveryDate: function(oEvent) {
-			// const oCurrentColumn = oEvent.getParameter("column");
-			// const oDeliveryDateColumn = this.byId("deliverydate");
-			// if (oCurrentColumn !== oDeliveryDateColumn) {
-			// 	oDeliveryDateColumn.setSortOrder(SortOrder.None); //No multi-column sorting
-			// 	return;
-			// }
-
-			// oEvent.preventDefault();
-
-			// const sOrder = oEvent.getParameter("sortOrder");
-			// const oDateFormat = DateFormat.getDateInstance({pattern: "dd/MM/yyyy"});
-
-			// this._resetSortingState(); //No multi-column sorting
-			// oDeliveryDateColumn.setSortOrder(sOrder);
-
-			// const oSorter = new Sorter(oDeliveryDateColumn.getSortProperty(), sOrder === SortOrder.Descending);
-			// //The date data in the JSON model is string based. For a proper sorting the compare function needs to be customized.
-			// oSorter.fnCompare = function(a, b) {
-			// 	if (b == null) {
-			// 		return -1;
-			// 	}
-			// 	if (a == null) {
-			// 		return 1;
-			// 	}
-
-			// 	const aa = oDateFormat.parse(a).getTime();
-			// 	const bb = oDateFormat.parse(b).getTime();
-
-			// 	if (aa < bb) {
-			// 		return -1;
-			// 	}
-			// 	if (aa > bb) {
-			// 		return 1;
-			// 	}
-			// 	return 0;
-			// };
-
-			// this.byId("table").getBinding().sort(oSorter);
-		},
-
 		_resetSortingState: function() {
 			const oTable = this.byId("table");
 			const aColumns = oTable.getColumns();
 			for (let i = 0; i < aColumns.length; i++) {
 				aColumns[i].setSortOrder(SortOrder.None);
 			}
-		}
+		},
+
+		onPressSupplierCell (oEvent) {
+			const oRow = oEvent.getParameter("row")
+
+			const SupplierID = this.getView().getModel().getProperty("SupplierID", oRow.getBindingContext());
+
+            this.oRouter.navTo(this.oRouteNames.details, {
+                SupplierID
+            });
+        }
     });
 });
